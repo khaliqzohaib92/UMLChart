2245e54500f99ce88e5aa58d311f5752cbd150d4
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-22 19:55:46 -0400

    Basic app structure with header, sidebar and canvas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import "./styles/index.scss";
-window.addEventListener("DOMContentLoaded", main =>{
+import paper, { Rectangle, Path, Point, Tool, PointText } from 'paper';
+import Sidebar from './scripts/sidebar';
 
+window.addEventListener("DOMContentLoaded", main =>{
+    // Get a reference to the canvas object
+    var canvas = document.getElementById('myCanvas');
+   
 });
+
+
---
+++ src/scripts/sidebar_data.js
@@ -0,0 +1,10 @@
+export default [
+ {
+     categoryName: "General",
+     shapes:[
+         {
+             
+         }
+     ]
+ }   
+]
